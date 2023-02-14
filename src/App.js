f5355801c54d553c260e327a5a5542133789306a
hafidaouzi0 <faouzibrahim2014@gmail.com>
2023-02-14 21:45:20 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
 const [InputText,SetText]=useState("")
 const [Tasks,SetTask]=useState([])
-const [colVal,Set]
+
 const GetText=(event)=>{
 SetText(event.target.value)
 }
@@ -14,7 +14,7 @@ const SubmitTask=()=>{
   const task={
     id: Tasks.length === 0 ? 1 : Tasks[Tasks.length - 1].id + 1,
     taskName:InputText,
-    completed:false
+    completed:false,
   }
 SetTask([...Tasks,task])
 }
@@ -22,7 +22,25 @@ const deleteTask=(id)=>{
     SetTask(Tasks.filter((task)=>{
      return task.id !== id}) )
 }
-  return (
+
+const CompleteTask=(id)=>{
+SetTask(
+Tasks.map((task)=>{
+if(task.id===id){
+  return {...task,completed:true}
+}
+else return task
+
+})
+
+)
+}
+
+
+
+
+ 
+return (
     <div className="App">
     <div className='AddTask'>
 <input onChange={GetText} type='text'/>
@@ -30,7 +48,7 @@ const deleteTask=(id)=>{
     </div>
 <div className='ListTask'>
 {Tasks.map((task)=>{
-  return (<Task taskName={task.taskName} id={task.id}  deleteTask={deleteTask}/>
+  return (<Task completed={task.completed} taskName={task.taskName} id={task.id} CompleteTask={CompleteTask} deleteTask={deleteTask} />
   )
 })}
     </div>
--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
 export const Task=(props)=>{
     return (<div>
-        <h1>{props.taskName}</h1>
-        <button onClick={ChangeColor} style={{color:colVal}}>Completed</button>
+        <h1 style={{backgroundColor:props.completed?"green":"white"}} >{props.taskName}</h1>
+        <button onClick={() => props.CompleteTask(props.id)} >Completed</button>
         <button onClick={() => props.deleteTask(props.id)}>X</button>
 
           </div>
