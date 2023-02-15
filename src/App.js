a3b9f650566a400888a71525b8f5332f0cd282ff
hafidaouzi0 <faouzibrahim2014@gmail.com>
2023-02-15 17:51:19 +0100

    oks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import Axios  from 'axios';
 function App() {
-
-const [catFact,SetCatFact]=useState("");
-
-  const FetchCat=()=>{
-    Axios.get("https://catfact.ninja/fact").then((res)=>{
-
-    console.log(res.data.fact)
-    SetCatFact(res.data.fact)
-    });
-  }
-
-  useEffect(()=>{
-    FetchCat()
-  },[])
-
 
 
 return (
     <div className="App">
-    
-  
-<button onClick={FetchCat}>Click to fetch cats</button>
+   <div  className='container'>
+ 
 
-<h1>{catFact}</h1>
+   </div>
 
     </div>
   );
---
+++ src/Cat.js
@@ -0,0 +1,25 @@
+// export const Cat=(props)=>{
+// const FetchCat=()=>{
+//     Axios.get("https://catfact.ninja/fact").then((res)=>{
+
+//     console.log(res.data.fact)
+//     SetCatFact(res.data.fact)
+//     });
+//   }
+
+//   useEffect(()=>{
+//     FetchCat()
+//   },[])
+
+
+//   return (
+//     <div className="App">
+    
+  
+// <button onClick={FetchCat}>Click to fetch cats</button>
+
+// <h1>{catFact}</h1>
+
+//     </div>
+//   );
+// }
